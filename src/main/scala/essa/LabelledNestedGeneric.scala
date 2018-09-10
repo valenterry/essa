f4a9e76882007e986e539bc3887adfad9e1c27f9
Valentin Willscher <valentin@willscher.de>
2018-09-10 17:42:36 +0200

    NestedGeneric was actually LabelledNestedGeneric and is now named properly

--- src/main/scala/essa/LabelledNestedGeneric.scala
+++ src/main/scala/essa/LabelledNestedGeneric.scala
@@ -4,18 +4,18 @@ import shapeless.labelled.FieldType
 import shapeless._
 
 /**
-  * Provides a generic representation, recursively for each field of every generic type and otherwise the type itself
+  * Provides a labelled generic representation, recursively for each field of every labelled generic type and otherwise the type itself
   */
-trait NestedGeneric[In] extends DepFn1[In] with Serializable {
+trait LabelledNestedGeneric[In] extends DepFn1[In] with Serializable {
   def to(in: In): Out = apply(in)
   def from(out: Out): In
 }
 
 trait LowPriorityNestedGeneric {
-  type Aux[In, Out0] = NestedGeneric[In] { type Out = Out0 }
+  type Aux[In, Out0] = LabelledNestedGeneric[In] { type Out = Out0 }
 
   implicit def notGeneric[In]: Aux[In, In] = {
-    new NestedGeneric[In] {
+    new LabelledNestedGeneric[In] {
       type Out = In
       override def apply(in: In): Out = in
       override def from(out: Out): In = out
@@ -23,23 +23,23 @@ trait LowPriorityNestedGeneric {
   }
 }
 
-object NestedGeneric extends LowPriorityNestedGeneric {
-  def apply[In](implicit ngen: NestedGeneric[In]): Aux[In, ngen.Out] = ngen
+object LabelledNestedGeneric extends LowPriorityNestedGeneric {
+  def apply[In](implicit lngen: LabelledNestedGeneric[In]): Aux[In, lngen.Out] = lngen
 
-  override type Aux[In, Out0] = NestedGeneric[In] { type Out = Out0 }
+  override type Aux[In, Out0] = LabelledNestedGeneric[In] { type Out = Out0 }
 
   implicit def hnil: Aux[HNil, HNil] =
-    new NestedGeneric[HNil] {
+    new LabelledNestedGeneric[HNil] {
       type Out = HNil
       override def apply(in: HNil): Out = HNil
       override def from(out: Out): HNil = HNil
     }
 
   implicit def hcons[HeadKey <: Symbol, HeadValue, Tail <: HList, HeadValueNG, TailNG <: HList](
-      implicit
-      ngHead: NestedGeneric.Aux[HeadValue, HeadValueNG],
-      ngTail: NestedGeneric.Aux[Tail, TailNG]): Aux[FieldType[HeadKey, HeadValue] :: Tail, FieldType[HeadKey, HeadValueNG] :: TailNG] =
-    new NestedGeneric[FieldType[HeadKey, HeadValue] :: Tail] {
+                                                                                                 implicit
+                                                                                                 ngHead: LabelledNestedGeneric.Aux[HeadValue, HeadValueNG],
+                                                                                                 ngTail: LabelledNestedGeneric.Aux[Tail, TailNG]): Aux[FieldType[HeadKey, HeadValue] :: Tail, FieldType[HeadKey, HeadValueNG] :: TailNG] =
+    new LabelledNestedGeneric[FieldType[HeadKey, HeadValue] :: Tail] {
       type Out = FieldType[HeadKey, HeadValueNG] :: TailNG
       override def apply(in: FieldType[HeadKey, HeadValue] :: Tail): Out = ngHead(in.head).asInstanceOf[FieldType[HeadKey, HeadValueNG]] :: ngTail(in.tail)
       override def from(out: Out): FieldType[HeadKey, HeadValue] :: Tail =
@@ -47,17 +47,17 @@ object NestedGeneric extends LowPriorityNestedGeneric {
     }
 
   implicit def cnil: Aux[CNil, CNil] =
-    new NestedGeneric[CNil] {
+    new LabelledNestedGeneric[CNil] {
       type Out = CNil
       def apply(in: CNil): Out          = unexpected
       override def from(out: Out): CNil = unexpected
     }
 
   implicit def ccons[HeadKey <: Symbol, HeadValue, Tail <: Coproduct, HeadValueNG, TailNG <: Coproduct](
-      implicit
-      ngHead: NestedGeneric.Aux[HeadValue, HeadValueNG],
-      ngTail: NestedGeneric.Aux[Tail, TailNG]): Aux[FieldType[HeadKey, HeadValue] :+: Tail, FieldType[HeadKey, HeadValueNG] :+: TailNG] =
-    new NestedGeneric[FieldType[HeadKey, HeadValue] :+: Tail] {
+                                                                                                         implicit
+                                                                                                         ngHead: LabelledNestedGeneric.Aux[HeadValue, HeadValueNG],
+                                                                                                         ngTail: LabelledNestedGeneric.Aux[Tail, TailNG]): Aux[FieldType[HeadKey, HeadValue] :+: Tail, FieldType[HeadKey, HeadValueNG] :+: TailNG] =
+    new LabelledNestedGeneric[FieldType[HeadKey, HeadValue] :+: Tail] {
       type Out = FieldType[HeadKey, HeadValueNG] :+: TailNG
       def apply(in: FieldType[HeadKey, HeadValue] :+: Tail): Out =
         in.eliminate(l => Inl(ngHead.apply(l).asInstanceOf[FieldType[HeadKey, HeadValueNG]]), r => Inr(ngTail.apply(r)))
@@ -68,16 +68,16 @@ object NestedGeneric extends LowPriorityNestedGeneric {
   implicit def deriveForNonEmptyProduct[In, InLGHead, InLGTail <: HList, InNG <: HList](
       implicit
       lgen: Lazy[LabelledGeneric.Aux[In, InLGHead :: InLGTail]],
-      ngen: Lazy[NestedGeneric.Aux[InLGHead :: InLGTail, InNG]]
+      lngen: Lazy[LabelledNestedGeneric.Aux[InLGHead :: InLGTail, InNG]]
   ): Aux[In, InNG] =
-    new NestedGeneric[In] {
+    new LabelledNestedGeneric[In] {
       type Out = InNG
-      def apply(in: In): Out          = ngen.value.apply(lgen.value.to(in))
-      override def from(out: Out): In = lgen.value.from(ngen.value.from(out))
+      def apply(in: In): Out          = lngen.value.apply(lgen.value.to(in))
+      override def from(out: Out): In = lgen.value.from(lngen.value.from(out))
     }
 
   implicit def deriveForEmptyProduct[In](implicit lgen: LabelledGeneric.Aux[In, HNil]): Aux[In, HNil] =
-    new NestedGeneric[In] {
+    new LabelledNestedGeneric[In] {
       type Out = HNil
       def apply(in: In): Out          = HNil
       override def from(out: Out): In = lgen.from(out)
@@ -86,11 +86,11 @@ object NestedGeneric extends LowPriorityNestedGeneric {
   implicit def deriveForNonEmptyCoproduct[In, InLG <: Coproduct, InNG <: Coproduct](
       implicit
       lgen: Lazy[LabelledGeneric.Aux[In, InLG]],
-      ngen: Lazy[NestedGeneric.Aux[InLG, InNG]]
+      lngen: Lazy[LabelledNestedGeneric.Aux[InLG, InNG]]
   ): Aux[In, InNG] =
-    new NestedGeneric[In] {
+    new LabelledNestedGeneric[In] {
       type Out = InNG
-      def apply(in: In): Out          = ngen.value.apply(lgen.value.to(in))
-      override def from(out: Out): In = lgen.value.from(ngen.value.from(out))
+      def apply(in: In): Out          = lngen.value.apply(lgen.value.to(in))
+      override def from(out: Out): In = lgen.value.from(lngen.value.from(out))
     }
 }
--- src/main/scala/essa/essa.scala
+++ src/main/scala/essa/essa.scala
@@ -3,9 +3,9 @@ package essa
 object essa {
   final class ConvertPartial[B] {
     def apply[A, NG](in: A)(implicit
-                                          ng: NestedGeneric.Aux[A, NG],
-                                          ng2: NestedGeneric.Aux[B, NG]): B = {
-      ng2.from(ng.to(in))
+                            lngenA: LabelledNestedGeneric.Aux[A, NG],
+                            lngenB: LabelledNestedGeneric.Aux[B, NG]): B = {
+      lngenB.from(lngenA.to(in))
     }
   }
 
